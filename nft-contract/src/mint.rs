f09e6cbfab12643dc5ddf8e65a82c2b34753e05f
Boban Damljanovic <boban.damljanovic@quantox.com>
2022-09-21 17:52:31 +0200

    first commit

---
+++ nft-contract/src/mint.rs
@@ -0,0 +1,42 @@
+use crate::*;
+
+#[near_bindgen]
+impl Contract {
+    #[payable]
+    pub fn nft_mint(
+        &mut self,
+        token_id: TokenId,
+        metadata: TokenMetadata,
+        receiver_id: AccountId,
+    ) {
+        //measure the initial storage being used on the contract
+        let initial_storage_usage = env::storage_usage();
+
+        //specify the token struct that contains the owner ID 
+        let token = Token {
+            owner_id: receiver_id,
+            //we set the approved account IDs to the default value (an empty map)
+            approved_account_ids: Default::default(),
+            //the next approval ID is set to 0
+            next_approval_id: 0,
+        };
+
+        //insert the token ID and token struct and make sure that the token doesn't exist
+        assert!(
+            self.tokens_by_id.insert(&token_id, &token).is_none(),
+            "Token already exists"
+        );
+
+        //insert the token ID and metadata
+        self.token_metadata_by_id.insert(&token_id, &metadata);
+
+        //call the internal method for adding the token to the owner
+        self.internal_add_token_to_owner(&token.owner_id, &token_id);
+
+        //calculate the required storage which was the used - initial
+        let required_storage_in_bytes = env::storage_usage() - initial_storage_usage;
+
+        //refund any excess storage if the user attached too much. Panic if they didn't attach enough to cover the required.
+        refund_deposit(required_storage_in_bytes);
+    }
+}
